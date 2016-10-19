2448f3cb4df50bb00ee5306cbf053e392699ea44
andre <andre@devel.devel>
2016-10-19 00:04:29 -0200

    Criando o mock com o @Mock.

--- src/test/java/com/codeatest/mockito/UsuariosServiceTest.java
+++ src/test/java/com/codeatest/mockito/UsuariosServiceTest.java
@@ -1,21 +1,21 @@
 package com.codeatest.mockito;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UsuariosServiceTest {
 
+	@Mock
 	private UsuariosRepository repository;
 
+	@InjectMocks
 	private UsuariosService service;
 
-	@Before
-	public void prepara() {
-		repository = Mockito.mock(UsuariosRepository.class);
-		service = new UsuariosService(repository);
-	}
-
+	
 	@Test
 	public void listaTodosUsuarios() {
 		service.todosUsuarios();
